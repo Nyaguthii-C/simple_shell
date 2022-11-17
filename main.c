d22a9aaad45995d2c0dba9c84b5e3cc187a6121d
Nyaguthii-C <nyaguthiigc@gmail.com>
2022-11-16 18:20:34 -0800

    display prompt and handle end of file

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+* main - prints simple shell  
+* 
+* Return: Always 0
+*
+*/
+int main (void)
+{
+	char *buf = NULL;
+	size_t size = 10;
+	ssize_t buf_read;
+
+	buf = malloc(sizeof(char) * size);
+
+	if (buf == NULL)
+	{
+		printf("Failed to allocate memory");
+		return (0);
+	}
+	while (buf_read != -1)
+	{
+		printf("$ ");
+		buf_read = getline(&buf, &size, stdin);
+		printf("%s\n", buf);
+	}
+	free(buf);
+	return (0);
+}
